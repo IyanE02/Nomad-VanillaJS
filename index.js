6205b7de4292d04fd3c33f16f25f464295f4d466
정윤혁 <superjyh02@naver.com>
2020-06-04 11:03:11 +0000

    day 3 quest

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-const title = document.getElementById("title");
+const title = document.getElementsByTagName("h2")[0];
 
-console.log(title);
+const superEventHandler = {
+  mouseOver: function() {
+    title.innerHTML = "Mouseover!";
+    title.style.color = colors[0];
+  },
+
+  mouseLeave: function() {
+    title.innerHTML = "Mouse left.";
+    title.style.color = colors[1];
+  },
+
+  click: function() {
+    title.innerHTML = "The title is clicked!";
+    title.style.color = colors[2];
+  },
+
+  resize: function() {
+    title.innerHTML = "The window is resized!";
+    title.style.color = colors[3];
+  },
+
+  contextMenu: function() {
+    title.innerHTML = "Right Click.";
+    title.style.color = colors[4];
+  }
+};
+
+title.addEventListener("mouseOver", superEventHandler.mouseOver);
+title.addEventListener("mouseLeave", superEventHandler.mouseLeave);
+title.addEventListener("click", superEventHandler.click);
+window.addEventListener("resize", superEventHandler.resize);
+window.addEventListener("contextMenu", superEventHandler.contextMenu);
